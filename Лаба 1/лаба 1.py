b21227f6290dd65eb93cddd8c91aca8ec2b0850e
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-13 17:20:54 +0300

    Update лаба 1.py

--- Лаба 1/лаба 1.py
+++ Лаба 1/лаба 1.py
@@ -3,7 +3,10 @@ from random import randint
 from statistics import median
 
 print("1 задание")
-k = 0
+sum_row = 0
 for i in range(0, 999999 + 1):
-    k += i
-print("Сумма ряда 0 - 999999 равна: ", k, '\n')
+    sum_row += i
+print("Сумма ряда 0 - 999999 равна: ", sum_row, '\n')
+print("2 задание")
+medium = sum_row / 1000000
+print("Среднее ряда: ", medium, '\n')
