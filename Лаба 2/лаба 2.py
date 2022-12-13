e787ceb3e4ed8f6f71377678ff6cadc054d3312c
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-13 17:28:38 +0300

    Update лаба 2.py

--- Лаба 2/лаба 2.py
+++ Лаба 2/лаба 2.py
@@ -5,3 +5,7 @@ class Footwear:
     def __init__(self, size, price):
         self.size = size
         self.price = price
+        
+    def Print(self):
+        print("Размер: ", self.size)
+        print("Цена: ", self.price)
