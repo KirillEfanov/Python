f3af693118e10cc7bcee988ac84b965ab741a264
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-13 17:29:09 +0300

    Update лаба 2.py

--- Лаба 2/лаба 2.py
+++ Лаба 2/лаба 2.py
@@ -9,3 +9,11 @@ class Footwear:
     def Print(self):
         print("Размер: ", self.size)
         print("Цена: ", self.price)
+        
+    def json_save(self, json_filepath: str):
+        to_json = {
+            "size": self.size,
+            "price": self.price
+        }
+        with open(json_filepath, 'w') as file:
+            json.dump(to_json, file)
