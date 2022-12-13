265543c7dc57243c9805eb6b1eb9afd78921e14b
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-13 17:18:31 +0300

    Update лаба 1.py

--- Лаба 1/лаба 1.py
+++ Лаба 1/лаба 1.py
@@ -1,3 +1,9 @@
 from math import prod
 from random import randint
 from statistics import median
+
+print("1 задание")
+k = 0
+for i in range(0, 999999 + 1):
+    k += i
+print("Сумма ряда 0 - 999999 равна: ", k, '\n')
