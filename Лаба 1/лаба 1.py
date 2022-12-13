d7c83f18cdf7a4ee0fdc9b1fa87e791b3dda9e92
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-13 17:22:03 +0300

    Update лаба 1.py

--- Лаба 1/лаба 1.py
+++ Лаба 1/лаба 1.py
@@ -10,3 +10,8 @@ print("Сумма ряда 0 - 999999 равна: ", sum_row, '\n')
 print("2 задание")
 medium = sum_row / 1000000
 print("Среднее ряда: ", medium, '\n')
+print("3 задание")
+random = []
+for i in range(1000000):
+    random.append(randint(1, 10))
+print("Медиана ряда 1000000 случайных чисел от 1 до 10: ", median(random), '\n')
