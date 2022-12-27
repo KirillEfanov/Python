953e851c4a2a91b61fa11a618df46de6617fb148
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-27 22:26:17 +0300

    Update laba4.py

--- лаба 4/laba4.py
+++ лаба 4/laba4.py
@@ -4,4 +4,8 @@ import requests
 from functools import reduce
 from bs4 import BeautifulSoup
 
+def map(list, function):
+    for i in range(len(list)):
+        list[i] = function(list[i])
+    return list
 
