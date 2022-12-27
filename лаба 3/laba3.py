43391e7a31b2a17ea97bdb7ef05d93401950e872
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-27 16:11:56 +0300

    Update laba3.py

--- лаба 3/laba3.py
+++ лаба 3/laba3.py
@@ -1,1 +1,5 @@
+from pyDatalog import pyDatalog
+from random import randrange
+import math
 
+pyDatalog.create_terms('sum_numbers, N, Sum_numbers, average_row, Average_row, row_random_numbers, median_random_numbers, Median_random_numbers, product, Product')
