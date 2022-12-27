728903b906f0e3b9019c33dd10a814551319c6cb
KirillEfanov <92543428+KirillEfanov@users.noreply.github.com>
2022-12-27 16:12:35 +0300

    Update laba3.py

--- лаба 3/laba3.py
+++ лаба 3/laba3.py
@@ -3,3 +3,10 @@ from random import randrange
 import math
 
 pyDatalog.create_terms('sum_numbers, N, Sum_numbers, average_row, Average_row, row_random_numbers, median_random_numbers, Median_random_numbers, product, Product')
+
+n = 1000000
+
+print("1 задание: ")
+sum_numbers[N] = N * N / 2
+print(sum_numbers[n] == Sum_numbers)
+print()
